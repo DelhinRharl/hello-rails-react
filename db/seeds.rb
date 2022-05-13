be8e1c5c19d0c61a0ef76b8d79701163c0f77a5c
DelhinRharl <delhinrharl@gmail.com>
2022-05-13 15:44:49 +0100

    Added components and created api

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: 'Hello there!')
+Greeting.create(message: 'Wagwaan gee')
+Greeting.create(message: 'Salam aleikum')
+Greeting.create(message: 'Shalom')
+Greeting.create(message: 'wys blud')
